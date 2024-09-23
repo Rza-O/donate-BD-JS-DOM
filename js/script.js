2021b8344c78ffd7c75e567a34c2990862fbbaad
appoclayptor <shahreza1414@gmail.com>
2024-09-23 23:52:35 +0600

    Made Noakhali portion Functional

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-// Declaring global variable
-// const myAmount = getTextFieldValue('my-amount');
-// Getting donate amount
-const donateBtn = document.getElementById('btn-donate');
+// Making Noakhali Donation money finctional
+const donateBtn = document.getElementById('btn-donate-noakhali');
 donateBtn.addEventListener('click', function(){
+    const myAmount = getTextFieldValue('my-amount');
     const donateAmount = getInputFieldById('donate-amount');
     if(donateAmount > 0 && !isNaN(donateAmount)){
-        console.log('good');
+        const noakhaliSum = getTextFieldValue('noakhali-sum') + donateAmount;
+        const remainingBalance = myAmount - donateAmount; 
+        document.getElementById('noakhali-sum').innerText = noakhaliSum;
+        console.log(noakhaliSum);
+        document.getElementById('my-amount').innerText = remainingBalance;
+        console.log(remainingBalance);
+        return;
     }
     else{
         alert("Invalid Input! Please try again!", location.reload());
