677cdbcccc46f7578afdd8138d2914e084b4ef5b
appoclayptor <shahreza1414@gmail.com>
2024-09-23 23:19:23 +0600

    Added alert for donate button and validated Modal

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
 // Declaring global variable
-const myAmount = getTextFieldValue('my-amount');
+// const myAmount = getTextFieldValue('my-amount');
 // Getting donate amount
 const donateBtn = document.getElementById('btn-donate');
 donateBtn.addEventListener('click', function(){
     const donateAmount = getInputFieldById('donate-amount');
-    if(donateAmount > 0 || !isNaN(donateAmount)){
-        // const noakhaliSumNumber = getTextFieldValue("noakhali-sum");
-        const remainingAmount = myAmount - donateAmount;
-        // document.getElementById('my-amount').innerText = remainingAmount;
-        console.log(remainingAmount);
-        
+    if(donateAmount > 0 && !isNaN(donateAmount)){
+        console.log('good');
     }
     else{
-        const modalHide = document.getElementById('modal-6');
-        modalHide.classList.add('hidden');
         alert("Invalid Input! Please try again!", location.reload());
     }
 });
